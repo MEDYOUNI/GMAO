d4d69e1f3737cd3c3c30155d2d64f3897088029f
MEDYOUNI <medyouni.anis@gmail.com>
2020-04-15 17:15:00 +0200

    filter equipement by site et secteur

--- frontend/src/app/equipement/equipement-list/equipement-list.component.ts
+++ frontend/src/app/equipement/equipement-list/equipement-list.component.ts
@@ -3,11 +3,10 @@ import { EquipementService } from 'src/app/services/equipement.service';
 import { Router } from '@angular/router';
 import { Equipement } from 'src/app/models/equipement.model';
 import { Subscription } from 'rxjs';
-
-interface Food {
-  value: string;
-  viewValue: string;
-}
+import { LieuService } from 'src/app/services/lieu.service';
+import { Site } from 'src/app/models/site.model';
+import { Secteur } from 'src/app/models/secteur.model';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-equipement-list',
@@ -16,26 +15,54 @@ interface Food {
 })
 export class EquipementListComponent implements OnInit, OnDestroy {
 
-  foods: Food[] = [
-    {value: '0', viewValue: 'Dunkerque-Mardyck'},
-    {value: '1', viewValue: 'Florange'},
-    {value: '2', viewValue: 'Montataire'}
-  ];
-
   dataSource: Equipement[];
   loading = false;
   equipSubscription: Subscription;
   displayedColumns: string[] = ['nom', 'description', 'type', 'site', 'secteur', 'zone', 'flux', 'actions'];
+  siteSubscription: Subscription;
+  sites: Site[];
+  secteurSubscription: Subscription;
+  secteurs: Secteur[];
+  equipements: Equipement[];
+
   constructor(private equipementService: EquipementService,
+              private lieuService: LieuService,
               private router: Router) { }
+
   ngOnDestroy(): void {
+    this.siteSubscription.unsubscribe();
     this.equipSubscription.unsubscribe();
+    this.secteurSubscription.unsubscribe();
   }
 
   ngOnInit(): void {
+    this.loadSites();
+    this.loadSecteurs()
+    this.loadEquipements();
+  }
+  loadSites() {
+    this.siteSubscription = this.lieuService.siteSubject.subscribe(
+      (site: Site[]) => {
+        this.sites = site;
+      }
+    );
+    this.lieuService.getSite();
+    this.lieuService.emitSite();
+  }
+  loadSecteurs() {
+    this.secteurSubscription = this.lieuService.secteurSubject.subscribe(
+      (secteur: Secteur[]) => {
+        this.secteurs = secteur;
+      }
+    );
+    this.lieuService.getSecteur();
+    this.lieuService.emitSecteur();
+  }
+  loadEquipements() {
     this.loading = true;
     this.equipSubscription = this.equipementService.equipementSubject.subscribe((response: Equipement[]) => {
       if (response) {
+        this.equipements = response;
         this.dataSource = response;
         this.loading = false;
       }
@@ -53,5 +80,17 @@ export class EquipementListComponent implements OnInit, OnDestroy {
   onDeleteEquipement(equipement: Equipement) {
     this.equipementService.removeEquipement(equipement);
   }
-
+  onChangeSite(site: string) {
+    console.log(site);
+    this.dataSource = this.equipements.filter(isSelectedSite);
+    function isSelectedSite(element, index, array) {
+          return (element.site === site);
+       }
+  }
+  onChangeSecteur(secteur: string) {
+    this.dataSource = this.equipements.filter(isSelectedSecteur);
+    function isSelectedSecteur(element, index, array) {
+          return (element.secteur === secteur);
+       }
+  }
 }
